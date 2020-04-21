91c2fa2897aa9f66ff1adf8f53667cd6bf57ec51
Walter Van Herck <walter.vanherck@gmail.com>
2020-04-21 20:49:45 +0200

    Allow editing

--- viewmodel/ViewModel.cpp
+++ viewmodel/ViewModel.cpp
@@ -54,6 +54,62 @@ QVariant TreeViewModel::data(const QModelIndex &index, int role) const
     return QVariant();
 }
 
+bool TreeViewModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    int row = index.row();
+    int col = index.column();
+    if (index.isValid() && row < rowCount() && role == Qt::EditRole)
+    {
+        auto children = __root_item->GetChildren();
+        auto item = children[row];
+        switch (col)
+        {
+        case 0:
+        {
+            return false;
+        }
+        case 1:
+        {
+            auto stringitem = dynamic_cast<Model::StringItem *>(item);
+            if (stringitem)
+            {
+                auto str = value.toString();
+                stringitem->SetData(str.toStdString());
+                emit dataChanged(index, index);
+                return true;
+            }
+            break;
+        }
+        default:
+            return false;
+        }
+    }
+    return false;
+}
+
+Qt::ItemFlags TreeViewModel::flags(const QModelIndex & index) const
+{
+    int col = index.column();
+    if (col == 1)
+    {
+        return Qt::ItemIsEditable | QAbstractItemModel::flags(index);
+    }
+    return QAbstractItemModel::flags(index);
+}
+
+QVariant TreeViewModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (section) {
+        case 0:
+            return QString("Type");
+        case 1:
+            return QString("Value");
+        }
+    }
+    return QVariant();
+}
+
 QModelIndex TreeViewModel::index(int row, int column, const QModelIndex & parent) const
 {
     if (!parent.isValid() && row < rowCount() && column < columnCount())

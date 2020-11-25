f1f5cce230271b083d293d545a69c05e0719bfd8
Van Herck Walter <Walter.Vanherck@iter.org>
2020-11-25 11:23:10 +0000

    Add empty CLI application

---
+++ src/main/c++/cli/main.cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+* $HeadURL: $
+* $Id: $
+*
+* Project       : SUP - Sequencer
+*
+* Description   : Sequencer for operational procedures
+*
+* Author        : Walter Van Herck (IO)
+*
+* Copyright (c) : 2010-2020 ITER Organization,
+*                 CS 90 046
+*                 13067 St. Paul-lez-Durance Cedex
+*                 France
+*
+* This file is part of ITER CODAC software.
+* For the terms and conditions of redistribution or use of this software
+* refer to the file ITER-LICENSE.TXT located in the top level directory
+* of the distribution package.
+******************************************************************************/
+
+// Global header files
+
+#include <common/log-api.h>
+#include <iostream>
+
+// Local header files
+
+#include "parser/SequenceParser.h"
+
+// Constants
+
+#undef LOG_ALTERN_SRC
+#define LOG_ALTERN_SRC "sup::sequencer"
+
+// Type definition
+
+// Global variables
+
+// Function declaration
+
+// Function definition
+
+int main(int argc, char * argv[])
+{
+  std::cout << "Starting sequencer-cli .." << std::endl;
+  return 0;
+}
+
+#undef LOG_ALTERN_SRC

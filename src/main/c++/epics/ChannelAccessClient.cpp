186df6f1f2fc3c3952b70b44342bbf4ed05e1647
bbauvir <bertrand.bauvir@iter.org>
2020-12-15 07:14:06 +0000

    Add simple ChannelAccessRead Instruction

--- src/main/c++/epics/ChannelAccessClient.cpp
+++ src/main/c++/epics/ChannelAccessClient.cpp
@@ -29,8 +29,12 @@
 
 #include <common/log-api.h> // Syslog wrapper routines
 
+#include <common/AnyValue.h>
+#include <common/AnyValueHelper.h>
+
 //#include <common/ChannelAccessClient.h>
 #include <common/ChannelAccessHelper.h> // CA helper routines
+#include <common/AnyTypeToCA.h> // CA helper routines .. type conversion
 
 // Local header files
 
@@ -52,10 +56,51 @@ namespace sequencer {
  * @brief Xxx
  */
 
+class BlockingCAFetchNode : public Instruction
+{
+
+  private:
+
+    /**
+     * @brief CA channel identifier attribute.
+     */
+
+    chid _channel;
+
+    /**
+     * @brief Xxx
+     */
+
+    ExecutionStatus ExecuteSingleImpl (UserInterface * ui, Workspace * ws) override;
+
+  protected:
+
+  public:
+
+    /**
+     * @brief Constructor.
+     */
+
+    BlockingCAFetchNode (void);
+
+    /**
+     * @brief Destructor.
+     */
+
+    ~BlockingCAFetchNode (void) override;
+
+};
+
 class BlockingCAWriteNode : public Instruction
 {
 
   private:
+
+    /**
+     * @brief CA channel identifier attribute.
+     */
+
+    chid _channel;
 
     /**
      * @brief Xxx
@@ -96,12 +141,72 @@ static bool global_caclient_initialised_flag = RegisterInstruction_ChannelAccess
 bool RegisterInstruction_ChannelAccessClient (void)
 {
 
-  log_info("RegisterInstruction_ChannelAccessClient - Entering function");
+  { // CAFetch registration
+    auto constructor = []() { return static_cast<Instruction*>(new BlockingCAFetchNode ()); };
+    GlobalInstructionRegistry().RegisterInstruction("BlockingCAFetchNode", constructor);
+  }
 
-  auto constructor = []() { return static_cast<Instruction*>(new BlockingCAWriteNode ()); };
-  GlobalInstructionRegistry().RegisterInstruction("BlockingCAWriteNode", constructor);
+  { // CAWrite registration
+    auto constructor = []() { return static_cast<Instruction*>(new BlockingCAWriteNode ()); };
+    GlobalInstructionRegistry().RegisterInstruction("BlockingCAWriteNode", constructor);
+  }
 
   return true;
+
+}
+
+ExecutionStatus BlockingCAFetchNode::ExecuteSingleImpl (UserInterface * ui, Workspace * ws)
+{
+
+  (void)ui;
+  (void)ws;
+
+  if (HasAttribute("channel"))
+    {
+      log_info("BlockingCAFetchNode::ExecuteSingleImpl('%s') - Method called with service '%s' ..", GetName().c_str(), GetAttribute("channel").c_str());
+    }
+  else
+    {
+      AddAttribute("service", "SEQ-TEST:STRING");
+    }
+
+  if (HasAttribute("datatype"))
+    {
+      log_info("BlockingCAFetchNode::ExecuteSingleImpl('%s') - .. type '%s'", GetName().c_str(), GetAttribute("datatype").c_str());
+    }
+  else
+    {
+      AddAttribute("datatype", "{\"type\":\"string\"}");
+    }
+
+  // Create CA context
+  bool status = ccs::HelperTools::ChannelAccess::CreateContext();
+
+  if (status)
+    {
+      log_info("BlockingCAFetchNode::ExecuteSingleImpl('%s') - Connect to variable '%s' ..", GetName().c_str(), GetAttribute("channel").c_str());
+      //status = ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), _channel);
+      (void)ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), _channel);
+    }
+
+  ccs::types::AnyValue _value (GetAttribute("datatype").c_str());
+
+  if (status && ccs::HelperTools::ChannelAccess::IsConnected(_channel))
+    {
+      log_info("BlockingCAFetchNode::ExecuteSingleImpl('%s') - Fetch as type '%s' ..", GetName().c_str(), GetAttribute("datatype").c_str());
+      status = ccs::HelperTools::ChannelAccess::ReadVariable(_channel, ccs::HelperTools::AnyTypeToCAScalar(_value.GetType()), _value.GetInstance());
+    }
+
+  if (status)
+    {
+      ccs::types::string buffer;
+      _value.SerialiseInstance(buffer, ccs::types::MaxStringLength);
+      log_info("BlockingCAFetchNode::ExecuteSingleImpl('%s') - .. '%s' value", GetName().c_str(), buffer);
+    }
+
+  // ToDo - Write to workspace
+
+  return (status ? ExecutionStatus::SUCCESS : ExecutionStatus::FAILURE);
 
 }
 
@@ -126,7 +231,7 @@ ExecutionStatus BlockingCAWriteNode::ExecuteSingleImpl (UserInterface * ui, Work
     }
   else
     {
-      AddAttribute("datatype", "string");
+      AddAttribute("datatype", "{\"type\":\"string\"}");
     }
 
   if (HasAttribute("instance"))
@@ -135,30 +240,38 @@ ExecutionStatus BlockingCAWriteNode::ExecuteSingleImpl (UserInterface * ui, Work
     }
   else
     {
-      AddAttribute("instance", "undefined");
+      AddAttribute("instance", "\"undefined\"");
     }
 
   // Create CA context
   bool status = ccs::HelperTools::ChannelAccess::CreateContext();
 
-  chid channel = 0;
-
   if (status)
     {
       log_info("BlockingCAWriteNode::ExecuteSingleImpl('%s') - Connect to variable '%s' ..", GetName().c_str(), GetAttribute("channel").c_str());
-      //status = ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), channel);
-      (void)ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), channel);
+      //status = ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), _channel);
+      (void)ccs::HelperTools::ChannelAccess::ConnectVariable(GetAttribute("channel").c_str(), _channel);
     }
 
-  if (status && ccs::HelperTools::ChannelAccess::IsConnected(channel))
+  ccs::types::AnyValue _value (GetAttribute("datatype").c_str());
+
+  if (status)
     {
-      log_info("BlockingCAWriteNode::ExecuteSingleImpl('%s') - Write as string '%s' ..", GetName().c_str(), GetAttribute("instance").c_str());
-      status = ccs::HelperTools::ChannelAccess::WriteVariable(channel, DBR_STRING, const_cast<void*>(static_cast<const void*>(GetAttribute("instance").c_str())));
+      log_info("BlockingCAWriteNode::ExecuteSingleImpl('%s') - Write as type '%s' ..", GetName().c_str(), GetAttribute("datatype").c_str());
+      status = _value.ParseInstance(GetAttribute("instance").c_str());
+    }
+
+  if (status && ccs::HelperTools::ChannelAccess::IsConnected(_channel))
+    {
+      status = ccs::HelperTools::ChannelAccess::WriteVariable(_channel, ccs::HelperTools::AnyTypeToCAScalar(_value.GetType()), _value.GetInstance());
     }
 
   return (status ? ExecutionStatus::SUCCESS : ExecutionStatus::FAILURE);
 
 }
+
+BlockingCAFetchNode::BlockingCAFetchNode (void) : Instruction("BlockingCAFetchNode") {}
+BlockingCAFetchNode::~BlockingCAFetchNode (void) {}
 
 BlockingCAWriteNode::BlockingCAWriteNode (void) : Instruction("BlockingCAWriteNode") {}
 BlockingCAWriteNode::~BlockingCAWriteNode (void) {}

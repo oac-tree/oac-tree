0165dadc664ec2957c31528ab6f2aff175049769
Van Herck Walter <Walter.Vanherck@iter.org>
2021-03-03 14:27:13 +0000

    Add InstructionHelper function declarations

---
+++ src/main/c++/instructions/InstructionHelper.cpp
@@ -0,0 +1,78 @@
+/******************************************************************************
+* $HeadURL: $
+* $Id: $
+*
+* Project       : SUP - Sequencer
+*
+* Description   : Sequencer for operational procedures
+*
+* Author        : Walter Van Herck (IO)
+*
+* Copyright (c) : 2010-2020 ITER Organization,
+*                 CS 90 046
+*                 13067 St. Paul-lez-Durance Cedex
+*                 France
+*
+* This file is part of ITER CODAC software.
+* For the terms and conditions of redistribution or use of this software
+* refer to the file ITER-LICENSE.TXT located in the top level directory
+* of the distribution package.
+******************************************************************************/
+
+// Global header files
+
+#include <common/log-api.h>
+
+// Local header files
+
+#include "InstructionHelper.h"
+
+// Constants
+
+#undef LOG_ALTERN_SRC
+#define LOG_ALTERN_SRC "sup::sequencer"
+
+// Type definition
+
+namespace sup {
+
+namespace sequencer {
+
+namespace helper {
+
+// Global variables
+
+// Function declaration
+
+// Function definition
+
+const Instruction * FindInstruction(const std::vector<const Instruction *> & instructions,
+                                    const std::string & name_path)
+{
+  return nullptr;
+}
+
+
+Instruction * CloneInstruction(const Instruction * instruction)
+{
+  return nullptr;
+}
+
+bool InitialiseVariableAttributes(Instruction & instruction, const AttributeMap * attributes)
+{
+  return false;
+}
+
+} // namespace helper
+
+} // namespace sequencer
+
+} // namespace sup
+
+extern "C" {
+
+// C API function definitions
+
+} // extern C
+
+#undef LOG_ALTERN_SRC

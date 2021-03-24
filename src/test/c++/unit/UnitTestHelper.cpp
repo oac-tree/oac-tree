cf1593fdef35710bd439008be2e18703f46aebae
bbauvir <bertrand.bauvir@iter.org>
2021-03-24 08:00:12 +0000

    Integrate Counting instruction in unitTestHelper namespace

--- src/test/c++/unit/UnitTestHelper.cpp
+++ src/test/c++/unit/UnitTestHelper.cpp
@@ -24,16 +24,22 @@
 #include <common/SysTools.h>
 #include <common/log-api.h>
 
+#include <common/BasicTypes.h>
+
 // Local header files
+
+#include "Instruction.h"
+#include "InstructionRegistry.h"
+#include "UserInterface.h"
 
 #include "UnitTestHelper.h"
 
 // Constants
 
 #undef LOG_ALTERN_SRC
-#define LOG_ALTERN_SRC "sup::sequencer::LogUI"
+#define LOG_ALTERN_SRC "unit-test"
 
-// Type definition
+// Type declaration
 
 namespace sup {
 
@@ -43,9 +49,58 @@ namespace UnitTestHelper {
 
 static const char * ENV_TEST_RESOURCES_PATH_NAME = "TEST_RESOURCES_PATH";
 
-// Function declaration
+const std::string CounterInstruction::Type = "Counter";
+ccs::types::uint32 CounterInstruction::counter = 0u;
+
+static bool _initialise_instruction = sup::sequencer::RegisterGlobalInstruction<CounterInstruction>();
 
 // Function definition
+
+ccs::types::uint32 CounterInstruction::GetCount (void) { return counter; }
+
+sup::sequencer::ExecutionStatus CounterInstruction::ExecuteSingleImpl (sup::sequencer::UserInterface * ui, sup::sequencer::Workspace * ws)
+{ 
+
+  if (sup::sequencer::Instruction::HasAttribute("incr"))
+    {
+      ccs::types::uint32 incr = ccs::HelperTools::ToInteger(sup::sequencer::Instruction::GetAttribute("incr").c_str());
+      counter += incr;
+    }
+  else
+    {
+      counter++; 
+    }
+
+  return sup::sequencer::ExecutionStatus::SUCCESS; 
+
+}
+
+CounterInstruction::CounterInstruction() : sup::sequencer::Instruction(CounterInstruction::Type) { counter = 0u; }
+CounterInstruction::~CounterInstruction() { counter = 0u; }
+
+void MockUI::UpdateInstructionStatusImpl (const sup::sequencer::Instruction * instruction) {}
+int MockUI::GetUserChoiceImpl (const std::vector<std::string> & choices, const std::string & description)
+{
+  log_info("TestUI::GetUserChoiceImpl - Description '%s'", description.c_str());
+  return _choice; 
+}
+
+bool MockUI::GetUserValueImpl (::ccs::types::AnyValue & value, const std::string & description)
+{
+  log_info("TestUI::GetUserValueImpl - Description '%s'", description.c_str());
+  _type = value.GetType();
+  value = _value;
+  return _status;
+}
+
+::ccs::base::SharedReference<const ::ccs::types::AnyType> MockUI::GetType (void) const { return _type; }
+
+void MockUI::SetChoice (int choice) { _choice = choice; }
+void MockUI::SetStatus (bool status) { _status = status; }
+void MockUI::SetValue (::ccs::types::AnyValue & value) { _value = value; }
+
+MockUI::MockUI (void) {}
+MockUI::~MockUI (void) {}
 
 std::string GetFullTestFilePath(const std::string & filename)
 {

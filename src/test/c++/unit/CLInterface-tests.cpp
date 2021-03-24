0fc77201534817a4e213f4f65678bfa4cb05a998
Van Herck Walter <Walter.Vanherck@iter.org>
2021-03-24 20:04:21 +0000

    Unit tests for CLInterface

---
+++ src/test/c++/unit/CLInterface-tests.cpp
@@ -0,0 +1,224 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP Sequencer
+ *
+ * Description   : Unit test code
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+// Global header files
+
+#include <gtest/gtest.h> // Google test framework
+#include <common/log-api.h> // Syslog wrapper routines
+#include <common/CompoundType.h>
+#include <common/StringTools.h>
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+// Local header files
+
+#include "CLInterface.h"
+#include "Instruction.h"
+#include "InstructionRegistry.h"
+
+// Constants
+
+#undef LOG_ALTERN_SRC
+#define LOG_ALTERN_SRC "sup::sequencer"
+
+// Type definition
+
+using namespace sup::sequencer;
+
+class CLInterfaceTest : public ::testing::Test
+{
+  protected:
+    CLInterfaceTest();
+    virtual ~CLInterfaceTest();
+
+    CLInterface cli;
+    CLInterface cli_verbose;
+    std::unique_ptr<Instruction> wait;
+};
+
+class CinRedirector
+{
+  private:
+    std::streambuf * cin_buf;
+
+  public:
+    CinRedirector(std::istream & input_stream);
+    ~CinRedirector();
+};
+
+class CoutRedirector
+{
+  private:
+    std::streambuf * cout_buf;
+
+  public:
+    CoutRedirector(std::ostream & output_stream);
+    ~CoutRedirector();
+};
+
+// Function declaration
+
+// Global variables
+
+static ::ccs::log::Func_t __handler = ::ccs::log::SetStdout();
+
+// Function definition
+
+TEST_F(CLInterfaceTest, UpdateInstructionStatus)
+{
+  std::ostringstream output;
+  CoutRedirector redirect(output);
+  EXPECT_TRUE(output.str().empty());
+  cli.UpdateInstructionStatus(wait.get());
+  auto logged = output.str();
+  EXPECT_FALSE(logged.empty());
+  EXPECT_NE(logged.find(StatusToString(ExecutionStatus::NOT_STARTED)), std::string::npos);
+}
+
+TEST_F(CLInterfaceTest, StartSingleStep)
+{
+  std::ostringstream output;
+  CoutRedirector redirect(output);
+  EXPECT_TRUE(output.str().empty());
+  cli_verbose.StartSingleStep();
+  EXPECT_FALSE(output.str().empty());
+}
+
+TEST_F(CLInterfaceTest, EndSingleStep)
+{
+  std::ostringstream output;
+  CoutRedirector redirect(output);
+  EXPECT_TRUE(output.str().empty());
+  cli_verbose.EndSingleStep();
+  EXPECT_FALSE(output.str().empty());
+}
+
+TEST_F(CLInterfaceTest, GetUserValue)
+{
+  ::ccs::types::AnyValue val(::ccs::types::UnsignedInteger32);
+  std::istringstream input("567890");
+  CinRedirector redirect(input);
+  EXPECT_TRUE(cli.GetUserValue(val));
+  ::ccs::types::uint32 result = val;
+  EXPECT_EQ(result, 567890u);
+}
+
+TEST_F(CLInterfaceTest, GetUserValueBool)
+{
+  ::ccs::types::AnyValue val(::ccs::types::Boolean);
+  std::istringstream input("true");
+  CinRedirector redirect(input);
+  EXPECT_TRUE(cli.GetUserValue(val));
+  bool result = val;
+  EXPECT_EQ(result, true);
+}
+
+TEST_F(CLInterfaceTest, GetUserValueString)
+{
+  ::ccs::types::AnyValue val(::ccs::types::String);
+  std::istringstream input("Test string");
+  CinRedirector redirect(input);
+  EXPECT_TRUE(cli.GetUserValue(val));
+  char buffer[STRING_MAX_LENGTH];
+  EXPECT_TRUE(val.SerialiseInstance(buffer, STRING_MAX_LENGTH));
+  EXPECT_EQ(::ccs::HelperTools::StringCompare("Test string", buffer), 0);
+}
+
+TEST_F(CLInterfaceTest, GetUserValueParseError)
+{
+  ::ccs::types::AnyValue val(::ccs::types::UnsignedInteger32);
+  std::istringstream input("twenty-four");
+  CinRedirector redirect(input);
+  EXPECT_FALSE(cli.GetUserValue(val));
+}
+
+TEST_F(CLInterfaceTest, GetUserValueUnsupportedType)
+{
+  auto test_type = (new ::ccs::types::CompoundType("CompoundTestType"))
+                      ->AddAttribute("timestamp", "uint64")
+                      ->AddAttribute("count", "uint16");
+  ::ccs::types::AnyValue val(test_type);
+  std::istringstream input("17");
+  CinRedirector redirect(input);
+  EXPECT_FALSE(cli.GetUserValue(val));
+}
+
+TEST_F(CLInterfaceTest, GetUserChoice)
+{
+  std::vector<std::string> choices = { "one", "two" };
+  std::istringstream input("1");
+  CinRedirector redirect(input);
+  auto choice = cli.GetUserChoice(choices);
+  EXPECT_EQ(choice, 1);
+}
+
+TEST_F(CLInterfaceTest, GetUserChoiceParseError)
+{
+  std::vector<std::string> choices = { "one", "two" };
+  std::istringstream input("one");
+  CinRedirector redirect(input);
+  auto choice = cli.GetUserChoice(choices);
+  EXPECT_EQ(choice, -1);
+}
+
+TEST_F(CLInterfaceTest, GetUserChoiceOutOfBounds)
+{
+  std::vector<std::string> choices = { "one", "two" };
+  std::istringstream input("2");
+  CinRedirector redirect(input);
+  auto choice = cli.GetUserChoice(choices);
+  EXPECT_EQ(choice, -1);
+}
+
+CLInterfaceTest::CLInterfaceTest()
+  : cli{}
+  , cli_verbose{true}
+  , wait{GlobalInstructionRegistry().Create("Wait")}
+{
+}
+
+CLInterfaceTest::~CLInterfaceTest()
+{}
+
+CinRedirector::CinRedirector(std::istream & input_stream)
+  : cin_buf{std::cin.rdbuf()}
+{
+  std::cin.rdbuf(input_stream.rdbuf());
+}
+
+CinRedirector::~CinRedirector()
+{
+  std::cin.rdbuf(cin_buf);
+}
+
+CoutRedirector::CoutRedirector(std::ostream & output_stream)
+  : cout_buf{std::cout.rdbuf()}
+{
+  std::cout.rdbuf(output_stream.rdbuf());
+}
+
+CoutRedirector::~CoutRedirector()
+{
+  std::cout.rdbuf(cout_buf);
+}
+
+#undef LOG_ALTERN_SRC

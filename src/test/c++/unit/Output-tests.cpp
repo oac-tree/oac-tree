1b9be0b8837bca7b15d2d620fea178301dd99484
Gennady Pospelov <gennady.pospelov@gmail.com>
2021-06-09 08:58:06 +0200

    Add unit test for Output instruction

---
+++ src/test/c++/unit/Output-tests.cpp
@@ -0,0 +1,108 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP Sequencer
+ *
+ * Description   : Unit test code
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+// Global header files
+
+#include <gtest/gtest.h>
+
+#include <common/BasicTypes.h>
+
+#include <SequenceParser.h>
+
+// Local header files
+
+#include "UnitTestHelper.h"
+#include "UserInterface.h"
+
+namespace {
+
+const std::string header {
+R"RAW(<?xml version="1.0" encoding="UTF-8"?>
+<Procedure xmlns="http://codac.iter.org/sup/sequencer" version="1.0"
+           name="Trivial procedure for testing purposes"
+           xmlns:xs="http://www.w3.org/2001/XMLSchema-instance"
+           xs:schemaLocation="http://codac.iter.org/sup/sequencer sequencer.xsd">)RAW"
+};
+
+const std::string footer {R"RAW(</Procedure>)RAW"};
+
+std::string CreateProcedureString(const std::string& body) {
+  return header + body + footer;
+}
+
+} // namespace
+
+//! Testing Output instruction.
+
+class OutputTest : public ::testing::Test {
+public:
+
+  //! Test interface which accepts values from the workspace.
+  class TestInterface : public ::sup::sequencer::UserInterface {
+  public:
+    TestInterface() : m_value(ccs::types::UnsignedInteger32){
+      m_value = static_cast<ccs::types::uint32>(0u);
+    }
+
+    bool PutValueImpl(const ::ccs::types::AnyValue &value,
+                      const std::string &description) override {
+      m_value = value;
+    }
+
+    bool GetUserValueImpl (::ccs::types::AnyValue &, const std::string&) override {
+      return true;
+    }
+
+    void UpdateInstructionStatusImpl(
+        const sup::sequencer::Instruction *instruction) override {}
+    ::ccs::types::AnyValue m_value;
+
+  };
+
+};
+
+// Takes integer value from the workspace and put in into TestInterface.
+
+TEST_F(OutputTest, PutInteger) {
+  static const std::string procedure_body { 
+  R"RAW(
+  <Sequence>
+    <Output from="var1"/>
+  </Sequence>
+  <Workspace>
+    <Local name="var1" type='{"type":"uint32"}' value='42' />
+  </Workspace>
+)RAW"};
+
+  const auto procedure_string = CreateProcedureString(procedure_body);
+
+  TestInterface ui;
+  // initial state
+  EXPECT_EQ(static_cast<ccs::types::uint32>(ui.m_value), static_cast<ccs::types::uint32>(22));
+
+  auto proc = sup::sequencer::ParseProcedureString(procedure_string);
+  ASSERT_TRUE(proc.get() != nullptr);
+  ASSERT_TRUE(sup::UnitTestHelper::TryAndExecute(
+      proc, &ui, sup::sequencer::ExecutionStatus::SUCCESS));
+
+  // checking that Output instruction has propagated the value to the interface
+  EXPECT_EQ(static_cast<ccs::types::uint32>(ui.m_value), static_cast<ccs::types::uint32>(42));
+}

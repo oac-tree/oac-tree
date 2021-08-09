38bfaf9f49a505f9e4d1ec28f2045d93f088ab95
Gennady Pospelov <gennady.pospelov@gmail.com>
2021-08-09 17:15:11 +0200

    Cleanup in FileVariable-tests, switch to temporary file instead of resource folder

--- src/test/c++/unit/FileVariable-tests.cpp
+++ src/test/c++/unit/FileVariable-tests.cpp
@@ -19,168 +19,181 @@
  * of the distribution package.
  ******************************************************************************/
 
-// Global header files
-
-#include <gtest/gtest.h> // Google test framework
+#include <gtest/gtest.h>
 
 #include <common/AnyValueHelper.h>
 
-// Local header files
-
+#include "LogUI.h"
 #include "SequenceParser.h"
+#include "UnitTestHelper.h"
 #include "Variable.h"
 #include "VariableRegistry.h"
-#include "LogUI.h"
-
-// Constants
 
 #undef LOG_ALTERN_SRC
 #define LOG_ALTERN_SRC "unit-test"
 
-// Type declaration
-
-// Function declaration
-
-static inline bool Terminate (void)
-{
+static inline bool Terminate(void) {
   bool status = false;
-  if (ccs::HelperTools::Exist("/tmp/variable.bck"))
-  {
+  if (ccs::HelperTools::Exist("/tmp/variable.bck")) {
     status = (std::remove("/tmp/variable.bck") == 0);
   }
   return status;
 }
 
-// Function definition
+TEST(FileVariable, File_write) {
+  const std::string body{R"(
+    <Sequence>
+        <Copy name="copy"
+            input="input"
+            output="file"/>
+    </Sequence>
+    <Workspace>
+        <Local name="input"
+            type='{"type":"MyStruct","attributes":[{"value":{"type":"float32"}}]}'
+            value='{"value":0.0}'/>
+        <FileVariable name="file"
+            file="/tmp/variable.bck"/>
+    </Workspace>
+)"};
 
-TEST(FileVariable, File_write)
-{
-  auto proc = sup::sequencer::ParseProcedureFile("../resources/variable_file.xml");
+  const std::string file_name = "/tmp/variable_file.xml";
+  ::sup::UnitTestHelper::TemporaryTestFile test_file(
+      file_name, ::sup::UnitTestHelper::CreateProcedureString(body));
 
-  bool status = bool(proc);
+  auto proc = sup::sequencer::ParseProcedureFile(file_name);
+  ASSERT_TRUE(proc.get() != nullptr);
 
-  if (status)
-    {
-      sup::sequencer::LogUI ui;
-      proc->ExecuteSingle(&ui);
-      status = (proc->GetStatus() == sup::sequencer::ExecutionStatus::SUCCESS);
-    }
+  sup::sequencer::LogUI ui;
+  proc->ExecuteSingle(&ui);
+  EXPECT_EQ(proc->GetStatus(), sup::sequencer::ExecutionStatus::SUCCESS);
 
-  if (status)
-    {
-      status = ccs::HelperTools::Exist("/tmp/variable.bck");
-    }
+  EXPECT_TRUE(ccs::HelperTools::Exist("/tmp/variable.bck"));
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = ccs::HelperTools::ReadFromFile(&value, "/tmp/variable.bck");
-    }
+  EXPECT_TRUE(ccs::HelperTools::ReadFromFile(&value, "/tmp/variable.bck"));
 
-  if (status)
-    {
-      status = static_cast<bool>(value.GetType());
-    }
+  ASSERT_TRUE(static_cast<bool>(value.GetType()));
+  // TODO check variable
 
   Terminate();
-
-  // ToDo - Test variable
-
-  ASSERT_EQ(true, status);
 }
 
-TEST(FileVariable, Setup_error)
-{
-  auto variable = sup::sequencer::GlobalVariableRegistry().Create("FileVariable");
+TEST(FileVariable, Setup_error) {
+  auto variable =
+      sup::sequencer::GlobalVariableRegistry().Create("FileVariable");
 
   bool status = static_cast<bool>(variable);
 
-  if (status)
-    { // Missing mandatory attribute .. Setup implicit
-      //status = (false == variable->Setup());
-      status = variable->AddAttribute("irrelevant","undefined");
-    }
+  if (status) { // Missing mandatory attribute .. Setup implicit
+    // status = (false == variable->Setup());
+    status = variable->AddAttribute("irrelevant", "undefined");
+  }
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = ((false == variable->GetValue(value)) &&
-                (false == static_cast<bool>(value.GetType())));
-    }
+  if (status) {
+    status = ((false == variable->GetValue(value)) &&
+              (false == static_cast<bool>(value.GetType())));
+  }
 
   ASSERT_EQ(true, status);
 }
 
-TEST(FileVariable, File_error)
-{
-  auto variable = sup::sequencer::GlobalVariableRegistry().Create("FileVariable");
+TEST(FileVariable, File_error) {
+  auto variable =
+      sup::sequencer::GlobalVariableRegistry().Create("FileVariable");
 
   bool status = static_cast<bool>(variable);
 
-  if (status)
-    {
-      status = variable->AddAttribute("file","undefined");
-    }
+  if (status) {
+    status = variable->AddAttribute("file", "undefined");
+  }
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = ((false == variable->GetValue(value)) &&
-                (false == static_cast<bool>(value.GetType())));
-    }
+  if (status) {
+    status = ((false == variable->GetValue(value)) &&
+              (false == static_cast<bool>(value.GetType())));
+  }
 
   ASSERT_EQ(true, status);
 }
 
-TEST(FileVariable, File_attr)
-{
-  auto proc = sup::sequencer::ParseProcedureFile("../resources/variable_attr.xml");
+TEST(FileVariable, File_attr) {
+  const std::string body{R"(
+    <Sequence>
+        <!-- Expected datatype -->
+        <Copy name="default"
+            input="input"
+            output="file"/>
+        <Copy name="value"
+            input="value"
+            output="file.value"/>
+        <!-- Should be math expression -->
+        <Copy name="severity"
+            input="severity"
+            output="file.severity"/>
+    </Sequence>
+    <Workspace>
+        <Local name="input"
+            type='{"type":"MyStruct","attributes":[{"timestamp":{"type":"uint64"}},{"value":{"type":"float32"}},{"severity":{"type":"uint32"}}]}'
+            value='{"timestamp":0,"value":0.0,"severity":0}'/>
+        <Local name="value"
+            type='{"type":"float32"}'
+            value='0.1'/>
+        <Local name="severity"
+            type='{"type":"uint32"}'
+            value='7'/>
+        <FileVariable name="file"
+            file="/tmp/variable.bck"/>
+    </Workspace>
+)"};
+
+  const std::string file_name = "/tmp/variable_attr.xml";
+  ::sup::UnitTestHelper::TemporaryTestFile test_file(
+      file_name, ::sup::UnitTestHelper::CreateProcedureString(body));
+
+  auto proc = sup::sequencer::ParseProcedureFile(file_name);
 
   bool status = bool(proc);
 
-  if (status)
-    {
-      sup::sequencer::LogUI ui;
-      sup::sequencer::ExecutionStatus exec = sup::sequencer::ExecutionStatus::FAILURE;
+  if (status) {
+    sup::sequencer::LogUI ui;
+    sup::sequencer::ExecutionStatus exec =
+        sup::sequencer::ExecutionStatus::FAILURE;
 
-      do
-        {
-          proc->ExecuteSingle(&ui);
-          exec = proc->GetStatus();
-        }
-      while ((sup::sequencer::ExecutionStatus::SUCCESS != exec) &&
+    do {
+      proc->ExecuteSingle(&ui);
+      exec = proc->GetStatus();
+    } while ((sup::sequencer::ExecutionStatus::SUCCESS != exec) &&
              (sup::sequencer::ExecutionStatus::FAILURE != exec));
 
-      status = (sup::sequencer::ExecutionStatus::SUCCESS == exec);
-    }
+    status = (sup::sequencer::ExecutionStatus::SUCCESS == exec);
+  }
 
-  if (status)
-    {
-      status = ccs::HelperTools::Exist("/tmp/variable.bck");
-    }
+  if (status) {
+    status = ccs::HelperTools::Exist("/tmp/variable.bck");
+  }
 
   ccs::types::AnyValue value; // Placeholder
 
-  if (status)
-    {
-      status = ccs::HelperTools::ReadFromFile(&value, "/tmp/variable.bck");
-    }
+  if (status) {
+    status = ccs::HelperTools::ReadFromFile(&value, "/tmp/variable.bck");
+  }
 
-  if (status)
-    {
-      status = static_cast<bool>(value.GetType());
-    }
+  if (status) {
+    status = static_cast<bool>(value.GetType());
+  }
 
   // Test variable
-  if (status)
-    {
-      status = (ccs::HelperTools::HasAttribute(&value, "severity") &&
-                (ccs::types::UnsignedInteger32 == ccs::HelperTools::GetAttributeType(&value, "severity")) &&
-                (7u == ccs::HelperTools::GetAttributeValue<ccs::types::uint32>(&value, "severity")));
-    }
+  if (status) {
+    status = (ccs::HelperTools::HasAttribute(&value, "severity") &&
+              (ccs::types::UnsignedInteger32 ==
+               ccs::HelperTools::GetAttributeType(&value, "severity")) &&
+              (7u == ccs::HelperTools::GetAttributeValue<ccs::types::uint32>(
+                         &value, "severity")));
+  }
 
   Terminate();
 

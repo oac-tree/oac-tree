e76fad0a8e11bafc4053429deb3058852e983714
Gennady Pospelov <gennady.pospelov@gmail.com>
2021-08-18 15:49:14 +0200

    Add utils to write TreeData to XML

---
+++ src/main/c++/parser/TreeDataXmlWriteUtils.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2021 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+/**
+ * @file TreeDataXmlWriteUtils.h
+ * @brief Utility functions to write TreeData to XML strings and XML files.
+ */
+
+#ifndef _SEQ_TreeDataXmlWriteUtils_h_
+#define _SEQ_TreeDataXmlWriteUtils_h_
+
+#include <string>
+
+namespace sup
+{
+namespace sequencer
+{
+class TreeData;
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // _SEQ_TreeDataXmlWriteUtils_h_

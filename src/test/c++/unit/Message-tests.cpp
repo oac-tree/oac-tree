bbce054a120ac24375571c48946f437e84bc570a
Walter Van Herck <walter.vanherck@iter.org>
2021-12-01 11:31:00 +0000

    Add unit tests for Message instruction

---
+++ src/test/c++/unit/Message-tests.cpp
@@ -0,0 +1,89 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP Sequencer
+ *
+ * Description   : Unit test code
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2021 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "InstructionRegistry.h"
+#include "Message.h"
+#include "Procedure.h"
+#include "UserInterface.h"
+#include "Workspace.h"
+
+using namespace sup::sequencer;
+
+//! Testing Message instruction.
+class MessageTest : public ::testing::Test
+{
+public:
+  //! Test interface for Message method.
+  class TestInterface : public ::sup::sequencer::UserInterface
+  {
+  public:
+    TestInterface() = default;
+
+    void UpdateInstructionStatusImpl(const sup::sequencer::Instruction *instruction) override {}
+    void MessageImpl(const std::string& message) override { ui_message = message; }
+
+    std::string ui_message;
+  };
+protected:
+  Procedure proc;
+  TestInterface ui;
+  Workspace ws;
+};
+
+TEST_F(MessageTest, Setup)
+{
+  Message message_instr{};
+  EXPECT_FALSE(message_instr.Setup(proc));
+
+  message_instr.AddAttribute("name", "MyMessageInstruction");
+  EXPECT_FALSE(message_instr.Setup(proc));
+
+  message_instr.AddAttribute("text", "Hello world!");
+  EXPECT_TRUE(message_instr.Setup(proc));
+}
+
+TEST_F(MessageTest, Execute)
+{
+  std::string message("Hello Sequencer!");
+  Message message_instr{};
+  message_instr.AddAttribute("text", message);
+  EXPECT_TRUE(message_instr.Setup(proc));
+
+  message_instr.ExecuteSingle(&ui, &ws);
+  EXPECT_EQ(message_instr.GetStatus(), ExecutionStatus::SUCCESS);
+
+  EXPECT_EQ(ui.ui_message, message);
+}
+
+TEST_F(MessageTest, FromRegistry)
+{
+  std::string message("Hello Sequencer!");
+  auto message_instr = GlobalInstructionRegistry().Create("Message");
+  message_instr->AddAttribute("text", message);
+  EXPECT_TRUE(message_instr->Setup(proc));
+
+  message_instr->ExecuteSingle(&ui, &ws);
+  EXPECT_EQ(message_instr->GetStatus(), ExecutionStatus::SUCCESS);
+
+  EXPECT_EQ(ui.ui_message, message);
+}

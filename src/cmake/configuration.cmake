06ffc0fe27aa47062a1d73cd9ba79f70e375b27d
Rafael Graça Silva <rafael.g.silva@criticalsoftware.com>
2023-01-25 17:01:20 +0000

    Remove CODAC Enviorenment special behaviour and add CODAC Enviorenment to cmake prefix path when available and enabled
    This removes the linking and installing special behaviours when inside a CODAC system, project now relies on cmake config files installed by dependencies to find libraries

--- src/cmake/configuration.cmake
+++ src/cmake/configuration.cmake
@@ -3,29 +3,18 @@
 include(GNUInstallDirs)
 include(CTest)
 
-get_filename_component(SEQUENCER_PROJECT_DIR "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
-
-# detecting CODAC environment
-if (SEQUENCER_CODAC)
-  if (DEFINED ENV{CODAC_ROOT})
+# Detecting CODAC environment
+if (NOT NO_CODAC AND DEFINED ENV{CODAC_ROOT})
     message(STATUS "CODAC environment detected at $ENV{CODAC_ROOT}")
-  else()
-    message(FATAL "No CODAC environment detected")
-  endif()
+    list(APPEND CMAKE_PREFIX_PATH $ENV{CODAC_ROOT} $ENV{CODAC_ROOT}/common)
 else()
   message(STATUS "Compiling without CODAC")
 endif()
 
-# build settings
+# Build settings
 if (NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE "RelWithDebInfo")
 endif()
+
 set(LIBVERSION ${CMAKE_PROJECT_VERSION})
 set(LIBSOVERSION ${CMAKE_PROJECT_VERSION_MAJOR})
-
-# Directories
-if (SEQUENCER_CODAC)
-  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SEQUENCER_PROJECT_DIR}/target/bin)
-else()
-  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
-endif()

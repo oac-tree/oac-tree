40cf7de0f34059844b02dc61f0a007657f58b782
Rafael Graça Silva <rg-silva@criticalsoftware.com>
2023-10-23 23:39:59 +0100

    misc: remove mvn-sonar script
    Analysis builds are completely automated, a manual run of this script would
    overwrite the automated reports with incomplete information (No parasoft analysis)

--- mvn-sonar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#+======================================================================
-#+======================================================================
-
-mvn clean test -Dcoverage
-mvn sonar -DupdateSonar -DanalysisMode=publish -Dlogin=f212cb1196e89c8984098d31a4fa6c9a43cc7d07

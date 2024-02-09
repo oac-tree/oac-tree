8bb7edacaa60bd492309d876bf96bf25a679158c
Walter Van Herck <walter.vanherck@iter.org>
2024-02-09 12:54:21 +0000

    Move JobController from sup-auto-server here and fix

---
+++ src/lib/sup/sequencer/job_states.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_SEQUENCER_JOB_STATES_H_
+#define SUP_SEQUENCER_JOB_STATES_H_
+
+namespace sup
+{
+namespace sequencer
+{
+enum class JobState
+{
+  kInitial = 0,
+  kPaused,
+  kStepping,
+  kRunning,
+  kSucceeded,
+  kFailed,
+  kHalted
+};
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_JOB_STATES_H_
---
+++ src/lib/sup/sequencer/runner/job_commands.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/sequencer/job_commands.h>
+
+namespace sup
+{
+namespace sequencer
+{
+
+bool operator<=(JobCommand left, JobCommand right)
+{
+  return static_cast<int>(left) <= static_cast<int>(right);
+}
+
+}  // namespace sequencer
+
+}  // namespace sup

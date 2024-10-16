c3cc9363fb4ccb25d747c7b991047db5e2e7a589
Walter Van Herck <Walter.Vanherck@iter.org>
2024-10-16 12:16:30 +0000

    Add Attribute/Variable/WorkspaceInfo classes

---
+++ src/lib/sup/sequencer/runner/variable_info.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/sequencer/variable_info.h>
+
+namespace sup
+{
+namespace sequencer
+{
+
+VariableInfo::VariableInfo(const std::string& var_type, sup::dto::uint32 idx,
+                           std::vector<AttributeInfo> attributes)
+  : m_var_type{var_type}
+  , m_index{idx}
+  , m_attributes{std::move(attributes)}
+{}
+
+VariableInfo::~VariableInfo() = default;
+
+VariableInfo::VariableInfo(const VariableInfo&) = default;
+VariableInfo::VariableInfo(VariableInfo&&) = default;
+VariableInfo& VariableInfo::operator=(const VariableInfo&) = default;
+VariableInfo& VariableInfo::operator=(VariableInfo&&) = default;
+
+std::string VariableInfo::GetType() const
+{
+  return m_var_type;
+}
+
+sup::dto::uint32 VariableInfo::GetIndex() const
+{
+  return m_index;
+}
+
+std::vector<AttributeInfo> VariableInfo::GetAttributes() const
+{
+  return m_attributes;
+}
+
+bool operator==(const VariableInfo& left, const VariableInfo& right)
+{
+  return (left.GetType() == right.GetType())
+      && (left.GetIndex() == right.GetIndex())
+      && (left.GetAttributes() == right.GetAttributes());
+}
+
+bool operator!=(const VariableInfo& left, const VariableInfo& right)
+{
+  return !(left == right);
+}
+
+}  // namespace sequencer
+
+}  // namespace sup
---
+++ src/lib/sup/sequencer/variable_info.h
@@ -0,0 +1,78 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_SEQUENCER_VARIABLE_INFO_H_
+#define SUP_SEQUENCER_VARIABLE_INFO_H_
+
+#include <sup/sequencer/attribute_info.h>
+
+#include <sup/dto/basic_scalar_types.h>
+
+#include <string>
+#include <vector>
+
+namespace sup
+{
+namespace sequencer
+{
+/**
+ * @brief The VariableInfo class represents the static information of a Variable, including its
+ * type and attributes. It also contains an index that allows to uniquely identify the Variable
+ * within a workspace.
+ */
+class VariableInfo
+{
+public:
+  VariableInfo(const std::string& var_type, sup::dto::uint32 idx,
+               std::vector<AttributeInfo> attributes);
+  ~VariableInfo();
+
+  // Copy/move construction/assignment
+  VariableInfo(const VariableInfo& other);
+  VariableInfo(VariableInfo&& other);
+  VariableInfo& operator=(const VariableInfo& other);
+  VariableInfo& operator=(VariableInfo&& other);
+
+  std::string GetType() const;
+  sup::dto::uint32 GetIndex() const;
+  std::vector<AttributeInfo> GetAttributes() const;
+
+private:
+  std::string m_var_type;
+  sup::dto::uint32 m_index;
+  std::vector<AttributeInfo> m_attributes;
+};
+
+/**
+ * @brief Compare two VariableInfo objects for equality.
+ */
+bool operator==(const VariableInfo& left, const VariableInfo& right);
+
+/**
+ * @brief Compare two VariableInfo objects for inequality.
+ */
+bool operator!=(const VariableInfo& left, const VariableInfo& right);
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_VARIABLE_INFO_H_

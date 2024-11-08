80c1d5587b651c7c0c060e60970c399116a8a49b
Walter Van Herck <Walter.Vanherck@iter.org>
2024-11-08 12:05:51 +0000

    Add AsyncInputAdapter

---
+++ src/lib/sup/sequencer/async_input_adapter.h
@@ -0,0 +1,102 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_SEQUENCER_ASYNC_INPUT_ADAPTER_H_
+#define SUP_SEQUENCER_ASYNC_INPUT_ADAPTER_H_
+
+#include <sup/sequencer/i_user_input_future.h>
+#include <sup/sequencer/user_input_request.h>
+
+#include <sup/dto/basic_scalar_types.h>
+
+#include <functional>
+#include <future>
+#include <list>
+#include <map>
+#include <mutex>
+
+namespace sup
+{
+namespace sequencer
+{
+
+// Handles only a single user input request
+class AsyncInputAdapter
+{
+public:
+  using InputFunction = std::function<UserInputReply(const UserInputRequest&, sup::dto::uint64)>;
+  using InterruptFunction = std::function<void(sup::dto::uint64)>;
+  class Future;
+  explicit AsyncInputAdapter(InputFunction input_func, InterruptFunction interrupt_func);
+  ~AsyncInputAdapter();
+
+  // return a future that can be used to check if the result is ready and get it
+  std::unique_ptr<IUserInputFuture> AddUserInputRequest(const UserInputRequest& request);
+
+private:
+  // query readiness of a user input request
+  bool UserInputRequestReady(const Future& token) const;
+
+  // throws if the input was not ready or no request is active
+  UserInputReply GetReply(const Future& token);
+
+  // cancel a user input request
+  void CancelInputRequest(const Future& token);
+
+  sup::dto::uint64 GetNewRequestId();
+  void CleanUpUnused();
+  InputFunction m_input_func;
+  InterruptFunction m_interrupt_func;
+  std::map<sup::dto::uint64, std::future<UserInputReply>> m_requests;
+  sup::dto::uint64 m_last_request_id;
+  std::list<sup::dto::uint64> m_cancelled;
+  mutable std::mutex m_mtx;
+};
+
+class AsyncInputAdapter::Future : public IUserInputFuture
+{
+public:
+  Future(AsyncInputAdapter& input_handler, sup::dto::uint64 id);
+  ~Future();
+
+  // No move/copy ctor/assignment
+  Future(const Future&) = delete;
+  Future& operator=(const Future&) = delete;
+  Future(Future&& other) = delete;
+  Future& operator=(Future&& other) = delete;
+
+  sup::dto::uint64 GetId() const override;
+
+  bool IsValid() const override;
+  bool IsReady() const override;
+
+  UserInputReply GetValue() override;
+
+private:
+  AsyncInputAdapter& m_input_handler;
+  sup::dto::uint64 m_id;
+};
+
+}  // namespace sequencer
+
+}  // namespace sup
+
+#endif  // SUP_SEQUENCER_ASYNC_INPUT_ADAPTER_H_
---
+++ src/lib/sup/sequencer/base/async_input_adapter.cpp
@@ -0,0 +1,199 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Sequencer for operational procedures
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/sequencer/async_input_adapter.h>
+
+#include <sup/sequencer/exceptions.h>
+
+#include <chrono>
+#include <utility>
+
+namespace sup
+{
+namespace sequencer
+{
+
+AsyncInputAdapter::AsyncInputAdapter(InputFunction input_func, InterruptFunction interrupt_func)
+  : m_input_func{std::move(input_func)}
+  , m_interrupt_func{std::move(interrupt_func)}
+  , m_requests{}
+  , m_last_request_id{0}
+  , m_cancelled{}
+  , m_mtx{}
+{}
+
+AsyncInputAdapter::~AsyncInputAdapter()
+{
+  for (auto& request : m_requests)
+  {
+    m_interrupt_func(request.first);
+  }
+}
+
+std::unique_ptr<IUserInputFuture> AsyncInputAdapter::AddUserInputRequest(
+  const UserInputRequest& request)
+{
+  std::lock_guard<std::mutex> lk{m_mtx};
+  CleanUpUnused();
+  auto id = GetNewRequestId();
+  auto request_future = std::async(std::launch::async, m_input_func, request, id);
+  m_requests.emplace(std::make_pair(id, std::move(request_future)));
+  std::unique_ptr<IUserInputFuture> future{new Future{*this, id}};
+  return future;
+}
+
+bool AsyncInputAdapter::UserInputRequestReady(const Future& token) const
+{
+  if (!token.IsValid())
+  {
+    return false;
+  }
+  std::lock_guard<std::mutex> lk{m_mtx};
+  auto request_it = m_requests.find(token.GetId());
+  if (request_it == m_requests.end())
+  {
+    return false;
+  }
+  auto result = request_it->second.wait_for(std::chrono::seconds(0));
+  return result == std::future_status::ready;
+}
+
+UserInputReply AsyncInputAdapter::GetReply(const Future& token)
+{
+  if (!token.IsValid())
+  {
+    // TODO: check if throwing is the right solution here
+    const std::string error = "AsyncInputAdapter::GetReply(): called with invalid token";
+    throw InvalidOperationException{error};
+  }
+  const auto id = token.GetId();
+  std::lock_guard<std::mutex> lk{m_mtx};
+  CleanUpUnused();
+  auto request_it = m_requests.find(id);
+  if (request_it == m_requests.end())
+  {
+    const std::string error =
+      "AsyncInputAdapter::GetUserInput(): called with unknown token id: " + std::to_string(id);
+    throw InvalidOperationException{error};
+  }
+  auto result = request_it->second.wait_for(std::chrono::seconds(0));
+  if (result != std::future_status::ready)
+  {
+    const std::string error = "User input with id [" + std::to_string(id) + "] was not ready!";
+    throw InvalidOperationException{error};
+  }
+  auto response = request_it->second.get();
+  m_requests.erase(request_it);
+  return response;
+}
+
+void AsyncInputAdapter::CancelInputRequest(const Future& token)
+{
+  if (!token.IsValid())
+  {
+    return;
+  }
+  const auto id = token.GetId();
+  std::lock_guard<std::mutex> lk{m_mtx};
+  auto request_it = m_requests.find(id);
+  if (request_it == m_requests.end())
+  {
+    return;
+  }
+  m_interrupt_func(id);
+  m_cancelled.push_back(id);
+  CleanUpUnused();
+}
+
+sup::dto::uint64 AsyncInputAdapter::GetNewRequestId()
+{
+  ++m_last_request_id;
+  while (m_last_request_id == 0 || m_requests.find(m_last_request_id) != m_requests.end())
+  {
+    ++m_last_request_id;
+  }
+  return m_last_request_id;
+}
+
+void AsyncInputAdapter::CleanUpUnused()
+{
+  auto it = m_cancelled.begin();
+  while (it != m_cancelled.end())
+  {
+    auto request_it = m_requests.find(*it);
+    if (request_it == m_requests.end())
+    {
+      it = m_cancelled.erase(it);
+    }
+    else
+    {
+      auto result = request_it->second.wait_for(std::chrono::seconds(0));
+      if (result != std::future_status::timeout)
+      {
+        m_requests.erase(request_it);
+        it = m_cancelled.erase(it);
+      }
+      else
+      {
+        ++it;
+      }
+    }
+  }
+}
+
+AsyncInputAdapter::Future::Future(AsyncInputAdapter& input_handler, sup::dto::uint64 id)
+  : m_input_handler{input_handler}
+  , m_id{id}
+{}
+
+AsyncInputAdapter::Future::~Future()
+{
+  if (IsValid())
+  {
+    m_input_handler.CancelInputRequest(*this);
+  }
+}
+
+sup::dto::uint64 AsyncInputAdapter::Future::GetId() const
+{
+  return m_id;
+}
+
+bool AsyncInputAdapter::Future::IsValid() const
+{
+  return m_id != 0;
+}
+
+bool AsyncInputAdapter::Future::IsReady() const
+{
+  return m_input_handler.UserInputRequestReady(*this);
+}
+
+UserInputReply AsyncInputAdapter::Future::GetValue()
+{
+  auto result = m_input_handler.GetReply(*this);
+  m_id = 0;
+  return result;
+}
+
+}  // namespace sequencer
+
+}  // namespace sup

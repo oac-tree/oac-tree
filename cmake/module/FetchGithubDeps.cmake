a1b0dfca55089e756ce503ff32d5591b7b643c9d
Walter Van Herck <walter.vanherck@iter.org>
2025-02-26 16:28:16 +0000

    Add sup-utils as github dependency

--- cmake/module/FetchGithubDeps.cmake
+++ cmake/module/FetchGithubDeps.cmake
@@ -9,16 +9,16 @@ include(FetchContent)
 FetchContent_Declare(
   sup-dto
   GIT_REPOSITORY https://github.com/oac-tree/sup-dto.git
-  GIT_TAG b08a4d6749e7ee6982511b1517edfa064bfb06b0 # v1.5
+  GIT_TAG 6000d1e11b8fd44ed835a4923d7d135529e5e04d # v1.7dev
   OVERRIDE_FIND_PACKAGE
 )
 
-# sup-utils currently has a clash in target name unit-tests (with sup-dto)
-# FetchContent_Declare(
-#   sup-utils
-#   GIT_REPOSITORY https://github.com/oac-tree/sup-utils.git
-#   GIT_TAG 41070c713f4aa71da498f7325600d26abccbfaf2 # v1.5
-#   OVERRIDE_FIND_PACKAGE
-# )
+sup-utils currently has a clash in target name unit-tests (with sup-dto)
+FetchContent_Declare(
+  sup-utils
+  GIT_REPOSITORY https://github.com/oac-tree/sup-utils.git
+  GIT_TAG a289e1fcd8e33638d505226d6a2ae6ce9321bdfd # v1.6dev
+  OVERRIDE_FIND_PACKAGE
+)
 
 message(VERBOSE "Building dependencies from source")

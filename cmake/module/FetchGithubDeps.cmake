b092685b69f8f14c970eef36308b72f761111836
Walter Van Herck <walter.vanherck@iter.org>
2025-02-27 16:18:17 +0000

    Switch to develop branch for fetching dependencies

--- cmake/module/FetchGithubDeps.cmake
+++ cmake/module/FetchGithubDeps.cmake
@@ -11,13 +11,13 @@ include(FetchContent)
 FetchContent_Declare(
   sup-dto
   GIT_REPOSITORY https://github.com/oac-tree/sup-dto.git
-  GIT_TAG 6000d1e11b8fd44ed835a4923d7d135529e5e04d # v1.7dev
+  GIT_TAG develop
   OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
   sup-utils
   GIT_REPOSITORY https://github.com/oac-tree/sup-utils.git
-  GIT_TAG a289e1fcd8e33638d505226d6a2ae6ce9321bdfd # v1.6dev
+  GIT_TAG develop
   OVERRIDE_FIND_PACKAGE
 )
